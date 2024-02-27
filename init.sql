074787a8ae85cf8491b691d892ab331b2b8d573b
LETomppa <vilpponent@gmail.com>
2024-02-27 14:55:07 +0200

    created School, Student and Field tables

--- init.sql
+++ init.sql
@@ -6,5 +6,13 @@ CREATE TABLE Student (
     studentID INT PRIMARY KEY NOT NULL,
     email VARCHAR(50),
     first_name VARCHAR(50),
-    
+    last_name VARCHAR(50),
+    birthday VARCHAR(10),
+    phone_number VARCHAR(20),
+);
+
+CREATE TABLE Field (
+    fieldID INT PRIMARY KEY NOT NULL,
+    guild_name VARCHAR(50),
+    name VARCHAR(50),
 );
