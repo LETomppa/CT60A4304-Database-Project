4a2c642e893f6e07889dcb4791b4c5b11f6093fc
LETomppa <vilpponent@gmail.com>
2024-02-27 15:03:43 +0200

    Foregn key added

--- init.sql
+++ init.sql
@@ -3,6 +3,12 @@ CREATE TABLE School (
     school_name VARCHAR(50),
 );
 
+CREATE TABLE Field (
+    fieldID INT PRIMARY KEY NOT NULL,
+    guild_name VARCHAR(50),
+    name VARCHAR(50),
+);
+
 CREATE TABLE Student (
     studentID INT PRIMARY KEY NOT NULL,
     email VARCHAR(50),
@@ -10,10 +16,8 @@ CREATE TABLE Student (
     last_name VARCHAR(50),
     birthday VARCHAR(10),
     phone_number VARCHAR(20),
-);
-
-CREATE TABLE Field (
-    fieldID INT PRIMARY KEY NOT NULL,
-    guild_name VARCHAR(50),
-    name VARCHAR(50),
+    schoolID INT NOT NULL,
+    fieldID INT NOT NULL,
+    FOREIGN KEY (schoolID) REFERENCES School(schoolID) ON DELETE CASCADE,
+    FOREIGN KEY (fieldID) REFERENCES Field(fieldID) ON DELETE CASCADE,
 );
