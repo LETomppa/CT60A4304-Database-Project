7a15db0c13f2b0b02be9f5e7494b8d4bae69bfd4
AlluNatu <aleksi.natunen@hotmail.fi>
2024-02-27 15:07:07 +0200

    Professor

--- init.sql
+++ init.sql
@@ -3,21 +3,15 @@ CREATE TABLE School (
     school_name VARCHAR(50),
 );
 
-CREATE TABLE Field (
-    fieldID INT PRIMARY KEY NOT NULL,
-    guild_name VARCHAR(50),
-    name VARCHAR(50),
-);
-
 CREATE TABLE Student (
     studentID INT PRIMARY KEY NOT NULL,
     email VARCHAR(50),
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    birthday VARCHAR(10),
-    phone_number VARCHAR(20),
-    schoolID INT NOT NULL,
-    fieldID INT NOT NULL,
-    FOREIGN KEY (schoolID) REFERENCES School(schoolID) ON DELETE CASCADE,
-    FOREIGN KEY (fieldID) REFERENCES Field(fieldID) ON DELETE CASCADE,
+    birthday VARCHAR(20),
+    courseID_FK INT,
+        FOREIGN KEY (Course)
+        REFERENCES Course (courseID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
