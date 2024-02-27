e75f5b4824346f025c7a88eb35d660540fc6cce1
AlluNatu <aleksi.natunen@hotmail.fi>
2024-02-27 15:08:51 +0200

    Professor

--- init.sql
+++ init.sql
@@ -15,3 +15,18 @@ CREATE TABLE Student (
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+CREATE TABLE Professor (
+    staffID INT PRIMARY KEY NOT NULL,
+    phone_number INT,
+    email VARCHAR(50),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    birthday VARCHAR(20),
+    courseID_FK INT,
+        FOREIGN KEY (Course)
+        REFERENCES Course (courseID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
